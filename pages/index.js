71a5d2e571fe5a69e568f35b3a34f226805b3beb
maryam Rezayee <mari.re9978@gmail.com>
2022-01-27 18:37:21 +0330

    intial footer and bottomNavigation section

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import styles from '../styles/Home.module.css'
 import Cart from "../pages/home/Cart/Cart"
+import Footer from '../components/footer/footer';
+import BottomNav from '../components/BottomNavigation/BottonNavigation';
 export default function Home() {
   return (
     <div className={styles.container}>
-      Home
+        <Footer/>
+        <BottomNav ItemCard={'100'}/>
       <Cart/>
     </div>
   )
