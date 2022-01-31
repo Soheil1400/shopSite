6f1649e6e24042d7b4cac98268386e9e4487d614
maryam Rezayee <mari.re9978@gmail.com>
2022-01-31 20:29:11 +0330

    adding some emotion style to category List

--- components/categoryList/categoryList.jsx
+++ components/categoryList/categoryList.jsx
@@ -1,9 +1,9 @@
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
-import Card from '@mui/material/Card';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import GridViewIcon from '@mui/icons-material/GridView';
 import Image from 'next/image'
-import CardContent from '@mui/material/CardContent';
 import Image1 from '../assets/CategoryListImage/automobile.png';
 import Image2 from '../assets/CategoryListImage/car.png';
 import Image3 from '../assets/CategoryListImage/desktop.png';
@@ -11,18 +11,56 @@ import Image4 from '../assets/CategoryListImage/electronic.png';
 import Image5 from '../assets/CategoryListImage/fashion.png';
 import Image6 from '../assets/CategoryListImage/fashion2.png';
 import Image7 from '../assets/CategoryListImage/tablet.png';
-const Item = styled(Paper)(({ theme }) => ({
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    // display:'flex',
-    // flexDirection:'row',
-    alignContent:'center',
-    boxShadow:'none',
-    color: theme.palette.text.secondary,
-  }));
+import { useState } from 'react';
+import Stack from '@mui/material/Stack';
+import Link from 'next/link'
+import { Typography } from '@mui/material';
+const PaperStyled=styled(Paper)({
+    width: '100%',
+    alignItems: 'center',
+    display:'flex',
+    justifyContent: 'flex-start',
+    textAlign:'left',
+    margin: '0.8rem 0.8rem',
+    padding:'10px',
+    boxShadow: ' rgb(43 52 71 / 9%) 0px 2px 2px 2px',
+    '&:hover': {
+        cursor:'pointer',
+        boxShadow: 'rgba(3,0,71,0.09) 3px 3px 3px 8px',
+     },
+});
+const StyledStack=styled(Stack)({
+    display:'flex',
+    flexDirection:'row'
+});
+const StyledGridIcon=styled(GridViewIcon)({
+    color:'#E94560',
+    width:'30px',
+    height:'30px',
+    marginBottom:'0.5rem',
+    marginLeft:'0.6rem'
+})
+const StyledCategoryTypography=styled(Typography)({
+    fontSize:'1.5rem',
+    fontWeight:'700',
+    color:'#2B3445',
+    marginLeft:'0.5rem'
+})
+const StyledViewAllTypography=styled(Typography)({
+    color:'#7D879C',
+    fontSize:'1rem'
+})
+const StyledArrowRightIcon=styled(ArrowRightIcon)({
+    color:'gray',
+    cursor:'pointer',
+})
+const CustomGrid=styled(Grid)({
+   display:'flex',
+   alignItem:'center',
+   justifyContent:'space-between'
+})
 const CategoryList = () => {
-    const listCategory=[
+    const [paper,setpaper]=useState([
         {id:'1',title:'Automobile',image:Image1},
         {id:'2',title:'Car',image:Image2},
         {id:'3',title:'Fashion',image:Image3},
@@ -35,27 +73,34 @@ const CategoryList = () => {
         {id:'10',title:'Electronic',image:Image3},
         {id:'11',title:'Furniture',image:Image5},
         {id:'12',title:'Camera',image:Image4},
-    ]
+    ])
     return (
-        <Grid container>
-           {listCategory.map((item)=>(
-                <Grid item xs={4} sm={3} md={2} key={item.id} >
-                      <Item>
-                         <Card sx={{display:'flex',flexDirection:'row'}} display={'flex'} flexDirection={'row'}>
-                                <CardContent display={'flex'} flexDirection={'row'} textAlign={'left'}>
-                                <Image
-                               src={item.image}
-                               alt={item.title}
-                               width={'100px'}
-                               loading='lazy'
-                               />
-                        <typography variant={'title'} style={{paddingBottom:'100px'}}>
+        <Grid container >
+            <CustomGrid container >
+                <StyledStack>
+                     <StyledGridIcon/>
+                    <StyledCategoryTypography variant={'h2'} >Categories</StyledCategoryTypography>
+                </StyledStack>
+                <StyledStack >
+                <StyledViewAllTypography>View all </StyledViewAllTypography>
+                  <Link href={"/#"}>
+                  <StyledArrowRightIcon/>
+                  </Link>
+                 </StyledStack>   
+            </CustomGrid>
+           {paper.map((item)=>(
+               <Grid container key={item.id} item xs={12} sm={4} md={2} lg={2} >
+                      <PaperStyled>
+                      <Image
+                        src={item.image}
+                        alt={item.title}
+                        loading='lazy'
+                     />
+                        <typography variant={'title'} >
                          {item.title}
                        </typography>
-                                  </CardContent>
-     </Card>
- </Item>
-                </Grid>
+                      </PaperStyled>
+               </Grid>    
            ))}
        </Grid>
     )
--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,8 @@ export default function Home() {
   return (
     <div className={styles.container}>
         <CategoryList/>
-        <Footer/>
-       <BottomNav ItemCard={'100'} className={'bottom_navigation'}/>
+        {/* <Footer/>
+       <BottomNav ItemCard={'100'} className={'bottom_navigation'}/> */}
       <Cart/>
     </div>
   )
