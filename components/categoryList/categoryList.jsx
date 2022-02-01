85f3d805f07a7f760d6dcbb410b96fee5245137c
maryam Rezayee <mari.re9978@gmail.com>
2022-02-01 19:52:55 +0330

    import color from theme.js

--- components/categoryList/categoryList.jsx
+++ components/categoryList/categoryList.jsx
@@ -1,16 +1,11 @@
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
+import Theme from '../../theme/theme';
 import Grid from '@mui/material/Grid';
 import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 import GridViewIcon from '@mui/icons-material/GridView';
 import Image from 'next/image'
-import Image1 from '../assets/CategoryListImage/automobile.png';
-import Image2 from '../assets/CategoryListImage/car.png';
-import Image3 from '../assets/CategoryListImage/desktop.png';
-import Image4 from '../assets/CategoryListImage/electronic.png';
-import Image5 from '../assets/CategoryListImage/fashion.png';
-import Image6 from '../assets/CategoryListImage/fashion2.png';
-import Image7 from '../assets/CategoryListImage/tablet.png';
+import Image1 from '../assets/CategoryListImage/car.png';
 import { useState } from 'react';
 import Stack from '@mui/material/Stack';
 import Link from 'next/link'
@@ -29,12 +24,12 @@ const PaperStyled=styled(Paper)({
         boxShadow: 'rgba(3,0,71,0.09) 3px 3px 3px 8px',
      },
 });
-const StyledStack=styled(Stack)({
+const StyledGrid=styled(Grid)({
     display:'flex',
     flexDirection:'row'
 });
 const StyledGridIcon=styled(GridViewIcon)({
-    color:'#E94560',
+    color:Theme.palette.primary.main,
     width:'30px',
     height:'30px',
     marginBottom:'0.5rem',
@@ -43,11 +38,11 @@ const StyledGridIcon=styled(GridViewIcon)({
 const StyledCategoryTypography=styled(Typography)({
     fontSize:'1.5rem',
     fontWeight:'700',
-    color:'#2B3445',
+    color:Theme.palette.secondary.dark,
     marginLeft:'0.5rem'
 })
 const StyledViewAllTypography=styled(Typography)({
-    color:'#7D879C',
+    color:Theme.palette.secondary.light,
     fontSize:'1rem'
 })
 const StyledArrowRightIcon=styled(ArrowRightIcon)({
@@ -62,31 +57,31 @@ const CustomGrid=styled(Grid)({
 const CategoryList = () => {
     const [paper,setpaper]=useState([
         {id:'1',title:'Automobile',image:Image1},
-        {id:'2',title:'Car',image:Image2},
-        {id:'3',title:'Fashion',image:Image3},
-        {id:'4',title:'Electronic',image:Image4},
-        {id:'5',title:'Mobile',image:Image5},
-        {id:'6',title:'Laptop',image:Image6},
-        {id:'7',title:'Desktop',image:Image7},
+        {id:'2',title:'Car',image:Image1},
+        {id:'3',title:'Fashion',image:Image1},
+        {id:'4',title:'Electronic',image:Image1},
+        {id:'5',title:'Mobile',image:Image1},
+        {id:'6',title:'Laptop',image:Image1},
+        {id:'7',title:'Desktop',image:Image1},
         {id:'8',title:'Tablet',image:Image1},
-        {id:'9',title:'Fashion',image:Image2},
-        {id:'10',title:'Electronic',image:Image3},
-        {id:'11',title:'Furniture',image:Image5},
-        {id:'12',title:'Camera',image:Image4},
+        {id:'9',title:'Fashion',image:Image1},
+        {id:'10',title:'Electronic',image:Image1},
+        {id:'11',title:'Furniture',image:Image1},
+        {id:'12',title:'Camera',image:Image1},
     ])
     return (
         <Grid container >
             <CustomGrid container >
-                <StyledStack>
+                <StyledGrid>
                      <StyledGridIcon/>
                     <StyledCategoryTypography variant={'h2'} >Categories</StyledCategoryTypography>
-                </StyledStack>
-                <StyledStack >
+                </StyledGrid>
+                <StyledGrid >
                 <StyledViewAllTypography>View all </StyledViewAllTypography>
                   <Link href={"/#"}>
                   <StyledArrowRightIcon/>
                   </Link>
-                 </StyledStack>   
+                 </StyledGrid>
             </CustomGrid>
            {paper.map((item)=>(
                <Grid container key={item.id} item xs={12} sm={4} md={2} lg={2} >
