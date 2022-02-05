6c3188d57e154f551fbf6c6d13fe8d9b64fa38a5
soheil <soheilghdimi@gmail.com>
2022-02-05 16:49:43 +0330

    V3.1 of TopCategories with css file added

--- pages/home/top_categories/TopCategories.jsx
+++ pages/home/top_categories/TopCategories.jsx
@@ -1,15 +1,17 @@
-import Box from '@mui/material/Box';
 import Image from 'next/image'
+import Box from '@mui/material/Box';
+import {Typography} from "@mui/material";
+import styled from "@emotion/styled";
+import CategoryIcon from '@mui/icons-material/Category';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+
 import Category1 from "../../../asset/category-1.png"
 import Category2 from "../../../asset/category-2.png"
 import Category3 from "../../../asset/category-3.png"
-import CategoryIcon from '@mui/icons-material/Category';
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import {Typography} from "@mui/material";
 
 
 const data =
@@ -48,14 +50,51 @@ const settings = {
     swipeToSlide: true,
 }
 
+const CartBox = styled(Box)(
+    {
+        minWidth: "384px",
+        minHeight: "152px",
+        p: 2,
+        borderRadius: "8px",
+        backgroundColor: 'rgb(255, 255, 255)',
+        boxShadow: 'rgb(3 0 71 / 9%) 0px 1px 3px',
+        cursor: "pointer",
+        margin: "10px",
+        display: "flex!important",
+        width: "auto!important",
+        justifyContent: 'center',
+    }
+)
+
+const TitlesBox = styled(Box)(
+    {
+        display: 'flex',
+        justifyContent: "space-between",
+        cursor: 'unset',
+        padding: '4px 10px',
+        position: 'absolute',
+        width: '340px',
+        top: '20%',
+    }
+)
+const Titles = styled(Typography)(
+    {
+        padding: "4px 10px",
+        borderRadius: "300px",
+        fontWeight: '600',
+        fontSize: '10px',
+        zIndex: '2',
+    }
+)
 
 export default function Category() {
     return (
-        <   Box style={{
+        <Box sx={{
             maxWidth: "1200px",
         }}>
 
-            <Box style={{
+            {/*injaro dast nazadam chon gharar bood hamoon default hamashoon biad roosh*/}
+            <Box sx={{
                 marginTop: "5px",
                 display: "flex!important",
                 justifyContent: "space-between",
@@ -92,52 +131,21 @@ export default function Category() {
 
                 {
                     data.map(item => (
-                        <Box key={item.id} component="span" sx={{
-                            minWidth: "384px",
-                            minHeight: "152px",
-                            p: 2,
-                            borderRadius: "8px",
-                            backgroundColor: 'rgb(255, 255, 255)',
-                            boxShadow: 'rgb(3 0 71 / 9%) 0px 1px 3px',
-                            cursor: "pointer",
-                            margin: "10px",
-                            display: "flex!important",
-                            width: "auto!important",
+                        <CartBox key={item.id} component="span">
 
-                        }}>
+                            <TitlesBox>
+                                <Titles
+                                    sx={{backgroundColor: 'rgb(15, 52, 96)', color: 'white',}}>
+                                    {item.title1}
+                                </Titles>
 
-                            <Box sx={{
-                                display: 'flex',
-                                justifyContent: "space-between",
-                                cursor: 'unset',
-                                padding: '4px 10px',
-                                position: 'absolute',
-                                zIndex: '1',
-                                width: '340px',
-                            }}>
-                                <Typography sx={{
-                                    backgroundColor: 'rgb(15, 52, 96)',
-                                    color: 'white',
-                                    padding: "4px 10px",
-                                    borderRadius: "300px",
-                                    fontWeight: '600',
-                                    fontSize: '10px',
-                                }}>{item.title1}</Typography>
-
-                                <Typography sx={{
-                                    backgroundColor: 'rgb(227, 233, 239)',
-                                    color: 'rgb(55, 63, 80)',
-                                    padding: "4px 10px",
-                                    borderRadius: "300px",
-                                    fontWeight: '600',
-                                    fontSize: '10px',
-                                }}>3k orders this week</Typography>
-                            </Box>
+                                <Titles sx={{backgroundColor: 'rgb(227, 233, 239)', color: 'rgb(55, 63, 80)',}}>
+                                    3k orders this week</Titles>
+                            </TitlesBox>
 
                             <Box
                                 sx={{
                                     display: "flex",
-                                    justifyContent: "center",
                                     alignItems: "center",
                                     position: "relative",
                                 }}
@@ -155,12 +163,12 @@ export default function Category() {
                                     bottom: "0",
                                     left: "0",
                                     opacity: "0",
-                                    "&:hover": {opacity: ".2", transition: 'all 250ms ease-in-out 0s'}
+                                    "&:hover": {opacity: ".3", transition: 'all 250ms ease-in-out 0s'}
                                 }}>{}</Box>
 
                             </Box>
 
-                        </Box>
+                        </CartBox>
                     ))
                 }
             </Slider>
