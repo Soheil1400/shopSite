b9d399d8d83b097ecf3fc46fd9a7238ddbc2de03
soheil <soheilghdimi@gmail.com>
2022-02-06 13:42:50 +0330

    V1.2 of SlideShow with css file added

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
-
+import './home/SlideShow/SlideShow.css'
 import Layout from '../components/layout/layout'
 
 export default function MyApp({ Component, pageProps }) {
