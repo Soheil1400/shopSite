03c7b7af9b07a1c6cad777baa0d491f66eab38ed
Mahdieh <mahdeh.abed@gmail.com>
2022-02-08 18:00:40 +0330

    redux cart

--- pages/home/Cart/CartSelectedProductList/CartSelectedProductsList.jsx
+++ pages/home/Cart/CartSelectedProductList/CartSelectedProductsList.jsx
@@ -1,18 +1,21 @@
-import { Stack } from "@mui/material";
+import { useSelector } from "react-redux";
+
+import { Stack} from "@mui/material";
+
 import CartSelectedProductCard from "../CartSelectedProductCard/CartSelectedProductCard";
-import {useSelector} from "react-redux";
 
 const CartSelectedProductsList = () => {
-  const items = useSelector((state) => state.cart.items)
+  const items = useSelector((state) => state.cart.items);
   return (
     <Stack>
       {items.map((product) => (
         <CartSelectedProductCard
           image={product.images[0].image}
-          number={product.number}
+          number={items.filter((i) => i.id === product.id)[0].count}
           price={product.price}
           name={product.name}
           key={product.id}
+          product={product}
         />
       ))}
     </Stack>
