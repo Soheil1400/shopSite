16e58920924c11390a11338571d509549250cbba
maryam Rezayee <mari.re9978@gmail.com>
2022-02-09 19:03:26 +0330

    changing image hover

--- components/topRatingAndFeature/CartItem.jsx
+++ components/topRatingAndFeature/CartItem.jsx
@@ -3,26 +3,25 @@ import {styled} from '@mui/material/styles';
 import { Typography ,Grid ,Paper,Box} from '@mui/material';
 import Image from 'next/image';
 import theme from '../../theme/theme';
-const StyledImage=styled(Image)({
+const StyledPaper=styled(Paper)({
+    position: "absolute",
+    backgroundColor: "black",
+    zIndex: "1",
+    top: "0",
+    right: "0",
+    bottom: "0",
+    left: "0",
+    opacity: "0",
     cursor:'pointer',
-    borderRadius: '10px',
-    position:'absolute',
-    opacity:'1',
-    '&:hover': {
-       opacity:'0.5'
-    }
-})
-const StyledGrid=styled(Box)({
-    backgroundColor:'gray',
-    borderRadius: '10px',
-    position:'relative',
+    borderRadius:'10px',
+    "&:hover": {opacity: ".3", transition: 'all 250ms ease-in-out 0s'},
 })
 const CartItem = ({image,title,rating,price,display,alignItem}) => {
     return (
         <Grid display={'flex'} alignItems={alignItem} flexDirection={'column'} p={1}>
-        <Grid>
-            <StyledGrid ></StyledGrid>
-            <StyledImage src={image} alt={title}  />
+        <Grid position={'relative'}>
+            <Image src={image} alt={title}/>
+            <StyledPaper>{}</StyledPaper>
         </Grid>
         <Grid display={display}>
            <Rating name="read-only" value={rating} readOnly precision={0.5}/>
--- components/topRatingAndFeature/RatingAndFeature.jsx
+++ components/topRatingAndFeature/RatingAndFeature.jsx
@@ -3,7 +3,7 @@ import TopRating from './topRating';
 import FeatureBrand from './featureBrand';
 const RatingAndFeature = () => {
     return ( 
-        <Grid container >
+        <Grid container spacing={3}>
             <Grid item xs={12} sm={12} md={12} lg={6} ><TopRating/></Grid>
             <Grid item xs={12} sm={12} md={12} lg={6} > <FeatureBrand/></Grid>
         </Grid>
